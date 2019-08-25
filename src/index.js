ccf8d5f78f9db0895f33c1c4d40fcbc6b22edfcd
marciaalegria <ytasegundo@gmail.com>
2019-08-25 14:05:53 -0400

    funcion nombre

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 document.getElementById("message").addEventListener("click",()=>{
     document.getElementById("message").value="";
-})
-
+});
+function myFunction() {
+    var final = document.getElementById("name").value;
+    var nuevo= "te envia un mensaje";
+    document.getElementById("final").innerHTML =  final+nuevo;
+  };
 
 document.getElementById("encode").addEventListener("click",() =>{
     cipher.encode();
-})
+});
 
 document.getElementById("decode").addEventListener("click",() =>{
     cipher.decode();
-})
+});
 
 document.getElementById("reset").addEventListener("click", () => {
     window.close();
